8a85b495105198bed10a5ec2423c0454518507dc
Mario Acevedo <jun_acevedo415@yahoo.com>
2020-07-28 06:50:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,13 +2,15 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
  
-require_relative './course.rb'
+require_relative
+'./course.rb'
  
 class Scraper
  
   def get_page
  
-    doc = Nokogiri::HTML(open("http:/
+      doc = 
+    Nokogiri::HTML(open("http:/
     /learn-co-
     curriculum.github.io/site-
     for-scraping/courses"))
